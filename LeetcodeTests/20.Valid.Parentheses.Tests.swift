75b9dcf23ad7b1d748dc7126d902610bf0f545e3
exbtn <thethunderwrath@gmail.com>
2023-03-13 13:24:04 +0000

    add soultion to valid parantheses problem

---
+++ LeetcodeTests/20.Valid.Parentheses.Tests.swift
@@ -0,0 +1,70 @@
+//
+//  Created by Yevhenii Veretennikov on 11/03/2023.
+//
+
+import XCTest
+
+final class ValidParenthesesTests: XCTestCase {
+    
+    fileprivate let solution = Solution()
+    fileprivate var string = ""
+    
+    private var testCases: [(String, Bool)] = [
+        ("([]{()})", true), ("()", true), ("()[]{}", true),
+        ("(]", false), ("]", false), ("(])", false)
+    ]
+    
+    func test_solution() {
+        testCases.forEach { (str, ans) in
+            XCTAssertEqual(solution.isValid(str), ans)
+        }
+    }
+    
+    func test_solution2() {
+        testCases.forEach { (str, ans) in
+            XCTAssertEqual(solution.isValid2(str), ans)
+        }
+    }
+}
+
+fileprivate class Solution {
+    
+    func isValid(_ s: String) -> Bool {
+        var backet = [Character]()
+        let match: [Character: Character] = [
+            ")": "(",
+            "}": "{",
+            "]": "["
+        ]
+
+        for char in s {
+            if let matching = match[char], let last = backet.last {
+                if last == matching {
+                    backet.removeLast()
+                } else {
+                    return false
+                }
+            } else {
+                backet.append(char)
+            }
+        }
+
+        return backet.isEmpty
+    }
+    
+    func isValid2(_ s: String) -> Bool {
+        var backet = [Character]()
+        
+        for char in s {
+            switch char {
+            case "(": backet.append(")")
+            case "{": backet.append("}")
+            case "[": backet.append("]")
+            default:
+                guard !backet.isEmpty, char == backet.removeLast() else { return false }
+            }
+        }
+        
+        return backet.isEmpty
+    }
+}
